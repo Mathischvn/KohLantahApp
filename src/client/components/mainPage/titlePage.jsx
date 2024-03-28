e05f520e10817f72d09d207310d9148d4583e170
Mathis CHAUVIN <mathischauvin256@gmail.com>
2024-03-28 13:01:57 +0100

    add main page and styles

---
+++ src/client/components/mainPage/titlePage.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./titlePage.css"
+export const TitlePage = () => {
+    return (
+        <>
+            <div className="titlePage">
+                <h1 className="">La quete du Livre 269</h1>
+                <div>
+                    <button className="start-button">
+                        Commencer
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
