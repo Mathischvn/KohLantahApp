03dca83dba838cd1d8395c5ef0de9f6df6d2f4b4
Bliztt22 <116337829+Bliztt22@users.noreply.github.com>
2024-04-02 13:41:46 +0200

    Update blankPage.jsx

--- src/client/components/blankPage/blankPage.jsx
+++ src/client/components/blankPage/blankPage.jsx
@@ -1,28 +1,30 @@
 import React from "react"
-import { FightPage } from "../../fightPage/fightPage.jsx"
-import { EnigmaPage } from "../../enigmaPage/enigmaPage.jsx"
-import { DicePage } from "../../dicePage/dicePage.jsx"
-import { ChoicePage } from "../../choicePage/choicePage.jsx"
+import FightPage  from "../../fightPage/fightPage.jsx"
+import EnigmaPage  from "../../enigmaPage/enigmaPage.jsx"
+import DicePage  from "../../dicePage/dicePage.jsx"
+import ChoicePage  from "../../choicePage/choicePage.jsx"
 import "./blankPage.css"
 export const BlankPage = () => {
     return (
         <>
             <div className="blankPage">
                 <h1 className="">section.libelle</h1>
-                { if section.combat != null }
-                    <FightPage/>
-                { endif }
-                { if section.enigme != null }
-                    <EngimaPage/>
-                { endif }
-                { if section.lancer_de != null }
-                    <DicePage/>
-                { endif }
-                { if section.choix != null }
-                    for(choix of section.choix){
-                        <ChoicePage/>
+                { 
+                    if section.combat != null {
+                        FightPage(section)
                     }
-                { endif }
+                    if section.enigme != null {
+                        EnigmaPage(section.id)
+                    }
+                    if section.lancer_de != null {
+                        DicePage(section.id)
+                    }
+                    if section.choix != null {
+                        for(choix of section.choix){
+                            ChoicePage(choix.id) 
+                        }
+                    }
+                }
             </div>
             <div><H1 className="">section.titre</H1></div>
         </>
