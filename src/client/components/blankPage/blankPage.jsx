31332eaf4160c7e51f3355592ac247464b9aa628
Bliztt22 <116337829+Bliztt22@users.noreply.github.com>
2024-04-02 11:48:52 +0200

    Update blankPage.jsx

--- src/client/components/blankPage/blankPage.jsx
+++ src/client/components/blankPage/blankPage.jsx
@@ -1,1 +1,17 @@
-
+import React from "react"
+import "./blankPage.css"
+export const BlankPage = () => {
+    return (
+        <>
+            <div className="blankPage">
+                <h1 className="">section.libelle</h1>
+                <div>
+                    <button className="choice-button">
+                        <p>choix.libelle</p>
+                    </button>
+                </div>
+            </div>
+            <div><H1 className="">section.titre</H1></div>
+        </>
+    )
+}
