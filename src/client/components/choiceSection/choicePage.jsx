df20b6accdc95a7d8ac8dde1260426b57bc7fef3
Bliztt22 <116337829+Bliztt22@users.noreply.github.com>
2024-04-02 14:03:34 +0200

    Update choicePage.jsx

--- src/client/components/choiceSection/choicePage.jsx
+++ src/client/components/choiceSection/choicePage.jsx
@@ -1,17 +1,11 @@
 import React from "react"
 import "./choicePage.css"
-export const ChoicePage = () => {
+export const ChoicePage = (choix) => {
     return (
         <>
-            <div className="titlePage">
-                <h1 className="">section.libelle</h1>
-                <div>
-                    <button className="choice-button">
-                        <p>choix.libelle</p>
-                    </button>
-                </div>
-            </div>
-            <div><H1 className="">section.titre</H1></div>
+            <button className="choice-button">
+                <p>choix.libelle</p>
+            </button>
         </>
     )
 }
