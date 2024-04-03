6841d9637ee53844e9cffa6d07be575301fd2424
Mathis CHAUVIN <mathischauvin256@gmail.com>
2024-04-03 14:16:14 +0200

    update

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,9 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-
+app.get('/select', (req, res) => {
+  res.send(resEntite);
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
