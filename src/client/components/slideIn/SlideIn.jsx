f306452c43684ea6a527cb3104412de45dc47624
nanicolas <nathan.nicolas@etudiant.univ-rennes1.fr>
2024-04-05 09:15:44 +0200

    slide out inventory

--- src/client/components/slideIn/SlideIn.jsx
+++ src/client/components/slideIn/SlideIn.jsx
@@ -2,11 +2,20 @@ import './SlideIn.css';
 const SlideIn = ({ children, startAnimation }) => {
   const transtionProperties = startAnimation
     ? { marginLeft: '0px', opacity: 1 }
-    : {};
-  return (
-    <div className="slide-in" style={transtionProperties}>
-      {children}
-    </div>
-  );
+    : { marginLeft: '-3000px', opacity: 1 };
+  if (startAnimation){
+    return (
+      <div className="slide-in" style={transtionProperties}>
+        {children}
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className="slide-out" style={transtionProperties}>
+        {children}
+      </div>
+    );
+  }
 };
 export default SlideIn;
