bdcec0668a522ffb773dee3ebdfaeb318103c778
krokolyo <kevin.hascoet21@gmail.com>
2024-04-05 09:59:04 +0200

    SlideIn - music

--- src/client/components/slideIn/SlideIn.jsx
+++ src/client/components/slideIn/SlideIn.jsx
@@ -1,5 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './SlideIn.css';
+import inventoryOpenSound from '/sound_effects/inventory-open.mp3?url'; // Importez le fichier audio
+import inventoryCloseSound from '/sound_effects/inventory-close.mp3?url'; // Importez le fichier audio
 import './SlideIn.css';
 const SlideIn = ({ children, startAnimation }) => {
+  function waitFiveSeconds() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(); // Résoudre la promesse après 5 secondes
+      }, 5000); // 5000 millisecondes = 5 secondes
+    });
+  }
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  useEffect(() => {
+    if (startAnimation && !isPlaying) {
+      const audio = new Audio(inventoryOpenSound);
+      audio.play();
+      setIsPlaying(true);
+      waitFiveSeconds()
+      setIsPlaying(false)
+    }
+    else if(!startAnimation && !isPlaying){
+      const audio = new Audio(inventoryCloseSound);
+      audio.play();
+      setIsPlaying(true);
+      waitFiveSeconds()
+      setIsPlaying(false)
+    }
+  }, [startAnimation, isPlaying]);
+
+
   const transtionProperties = startAnimation
     ? { marginLeft: '0px', opacity: 1 }
     : { marginLeft: '-3000px', opacity: 1 };
