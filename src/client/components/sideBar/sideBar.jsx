aedaff89f56f6e423677a41c55fe0f25ff3371da
lucas <lucasvaugrente35@gmail.com>
2024-05-29 15:56:29 +0200

    Update SideBar logo inventory

--- src/client/components/sideBar/sideBar.jsx
+++ src/client/components/sideBar/sideBar.jsx
@@ -6,13 +6,21 @@ import { InventoryPage } from "../inventoryPage/inventoryPage"
 
 export const SideBar = ({inventory, stats}) => {
     const [startAnimation, setStartAnimation] = useState(false);
+    const [closeInventory, setCloseInventory] = useState(false);
 
     return (
         <div>
             <SlideIn startAnimation={startAnimation}>
                 <InventoryPage inventory={inventory} stats={stats}/>
             </SlideIn>
-            <div className="side-bar" onClick={() => setStartAnimation(!startAnimation)}></div>
+            {!closeInventory && 
+            <div className="side-bar side-bar-open" onClick={() => {setStartAnimation(!startAnimation), setCloseInventory(true)}}>
+                <img className="inventory-image-open" src="/public/images/inventory.png" alt="Inventory Logo" title="Ouvrir l'inventaire"/>
+            </div>}
+            {closeInventory && 
+            <div className="side-bar side-bar-close" onClick={() => {setStartAnimation(!startAnimation), setCloseInventory(false)}}>
+                <img className="inventory-image-close" src="/public/images/croix.png" alt="Inventory Logo" title="Fermer l'inventaire"/>
+            </div>}
         </div>
     )
     
