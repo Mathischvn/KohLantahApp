73ba3b3a4c8b5f29dca96e0b2b689760b4ed4f4a
lucas <lucasvaugrente35@gmail.com>
2024-05-30 15:53:04 +0200

    Update Inventory

--- src/client/components/inventoryPage/inventoryPage.jsx
+++ src/client/components/inventoryPage/inventoryPage.jsx
@@ -6,31 +6,36 @@ import { ProfilePicture } from "../profilePicture/profilePicture"
 import { useState } from "react"
 
 export const InventoryPage = ({inventory, stats}) => {
+
+    const playerName = document.cookie.match(/(?<=name=)[^;]*/)[0];
+
     return (
         <>
             <div className="inventory">
-                <div className="inventory-left">
-                    <h1>Vos bijoux</h1>
-                        <InventoryBag bagSize={2} icon={"fa-solid fa-gem"}></InventoryBag>
-                    <h1>Vos artéfacts</h1>
-                        <InventoryBag bagSize={2} icon={"fa-solid fa-wand-sparkles"}></InventoryBag>
-                    <h1>Vos livres</h1>
-                        <InventoryBag bagSize={2} icon={"fa-solid fa-book-bookmark"}></InventoryBag>
+                <div className="player-profile">
+                    <h1 className="playerName">{playerName}</h1>
+                    <ProfilePicture></ProfilePicture>
+                    <InventoryBag bagSize={2} icon={"fa-solid fa-gem"} className={"jewelry-bag"}></InventoryBag>
+                    <InventoryBag bagSize={2} icon={"fa-solid fa-wand-sparkles"} className={"artifacts-bag"}></InventoryBag>
+                    <InventoryBag bagSize={2} icon={"fa-solid fa-book-bookmark"} className={"books-bagh"}></InventoryBag>
                 </div>
                 
-                <div className="inventory-right">
-                    <ProfilePicture></ProfilePicture>
-                    <h1>Vos statistiques</h1>
+                <div className="player-stats-inventory">
+                    <div>
+                        <h1>Vos statistiques</h1>
                         <div className="character-container">
-                            <StatBar stat={"Intelligence"} value={stats[0]} color={"blue"}></StatBar>
-                            <StatBar stat={"Force"} value={stats[1]} color={"red"}></StatBar>
-                            <StatBar stat={"Santé mentale"} value={stats[2]} color={"green"}></StatBar>
+                            <StatBar stat={"Intelligence"} value={stats[0]} color={"#5353c1"}></StatBar>
+                            <StatBar stat={"Force"} value={stats[1]} color={"#e32222"}></StatBar>
+                            <StatBar stat={"Santé mentale"} value={stats[2]} color={"#3db23d"}></StatBar>
                         </div>
+                    </div>
 
-                    <h1>Votre inventaire</h1>
+                    <div>
+                        <h1>Votre inventaire</h1>
                         <div className="main-bag">
                             <InventoryBag bagSize={16} inventory={inventory} stats={stats}></InventoryBag>
                         </div>
+                    </div>
                 </div>
             </div>
         </>
