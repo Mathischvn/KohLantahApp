cc737278f302ea5b4f83d26d8015ef6d11269641
lucas <lucasvaugrente35@gmail.com>
2024-06-04 14:29:22 +0200

    Update Security Connection

--- src/client/components/mainPage/titlePage.jsx
+++ src/client/components/mainPage/titlePage.jsx
@@ -11,20 +11,26 @@ export const TitlePage = () => {
     const createSave = () => {
         const inputName = document.querySelector(".player_name_input_create").value;
         const inputMdp = document.querySelector(".player_mdp_input_create").value;
-        if (inputName !== "" && inputMdp !== "") {
-            const fetchData = async () => { 
-                const response = await fetch(`http://localhost:3000/api/player/insertPlayer/${inputName}/${inputMdp}`);
-                const data = await response.json();
-                if(data.length === 0) {
-                    document.cookie = `name=${inputName}`;
-                    navigate("/game");
-                } else {
-                    const errorMessage = document.querySelector(".error-message");
-                    errorMessage.style.opacity = "1"
-                    errorMessage.innerHTMl = "Ce nom existe déjà, veuillez prendre un autre nom.";
+        if(verifyPassword()) {
+            if (inputName !== "" && inputMdp !== "") {
+                const fetchData = async () => { 
+                    const response = await fetch(`http://localhost:3000/api/player/insertPlayer/${inputName}/${inputMdp}`);
+                    const data = await response.json();
+                    if(data.length === 0) {
+                        document.cookie = `name=${inputName}`;
+                        navigate("/game");
+                    } else {
+                        const errorMessage = document.querySelector(".error-message");
+                        errorMessage.style.opacity = "1";
+                        errorMessage.innerHTML = "Ce nom existe déjà, veuillez prendre un autre nom.";
+                    }
                 }
+                fetchData();
             }
-            fetchData();
+        } else {
+            const errorMessage = document.querySelector(".error-message");
+            errorMessage.style.opacity = "1";
+            errorMessage.innerHTML = "Mots de passe différents";
         }
     };
 
@@ -32,25 +38,37 @@ export const TitlePage = () => {
         const inputName = document.querySelector(".player_name_input_loading").value;
         const inputMdp = document.querySelector(".player_mdp_input_loading").value;
         if (inputName !== "" && inputMdp !== "") {
-            const fetchData = async () => { 
-                const response = await fetch(`http://localhost:3000/api/player/getPlayer/${inputName}/${inputMdp}`);
-                console.log("CONNARD");
-                const data = await response.json();
-                console.log(data);
-                if(data.length !== 0) {
-                    document.cookie = `name=${inputName}`;
-                    navigate("/game");
-                } else {
-                    const errorMessage = document.querySelector(".error-message");
-                    errorMessage.style.opacity = "1";
-                    errorMessage.innerHTML = "Nom ou mot de passe incorrect";
+            document.querySelector(".start-button").innerHTML = 'Charger la partie <i class="fa-solid fa-spinner fa-spin-pulse"></i>';
+            setTimeout(() => {
+                const fetchData = async () => { 
+                    const response = await fetch(`http://localhost:3000/api/player/getPlayer/${inputName}/${inputMdp}`);
+                    const data = await response.json();
+                    if(data.length !== 0) {
+                        document.cookie = `name=${inputName}`;
+                        navigate("/game");
+                    } else {
+                        const errorMessage = document.querySelector(".error-message");
+                        errorMessage.style.opacity = "1";
+                        errorMessage.innerHTML = "Nom ou mot de passe incorrect";
+                    }
+                    document.querySelector(".start-button").innerHTML = 'Charger la partie <i class="fa-solid fa-play"></i>';
                 }
-            }
-            fetchData();
+                fetchData();
+            }, "500");              
         }
-    }
+    };
 
-    const clickButton = (bool, button) => {
+    const verifyPassword = () => {
+        const inputMdp = document.querySelector(".player_mdp_input_create").value;
+        const inputConfirmMdp = document.querySelector(".player_confirm_mdp_input_create").value;
+        let samePassword = true;
+        if(inputMdp !== inputConfirmMdp) {
+            samePassword = false;
+        }
+        return samePassword;
+    };
+
+    const changeMenu = (bool, button) => {
         setCreateLoadingSave(bool);
         setActiveButton(button);
         const inputNameCreate = document.querySelector(".player_name_input_create");
@@ -74,13 +92,13 @@ export const TitlePage = () => {
             <div className="buttons-title">
                 <button
                     className={activeButton === 'new' ? 'active' : ''}
-                    onClick={() => clickButton(false, 'new')}
+                    onClick={() => changeMenu(false, 'new')}
                 >
                     Créer une nouvelle partie
                 </button>
                 <button
                     className={activeButton === 'load' ? 'active' : ''}
-                    onClick={() => clickButton(true, 'load')}
+                    onClick={() => changeMenu(true, 'load')}
                 >
                     Charger une partie
                 </button>
@@ -88,18 +106,34 @@ export const TitlePage = () => {
             <p className="error-message"></p>
             {createLoadingSave ? (
                 <div className="container-title-page loading-save">
-                    <input className="player_name_input_loading" type="text" placeholder="Votre nom" />
-                    <input className="player_mdp_input_loading" type="text" placeholder="Votre mot de passe" />
+                    <div className="blockInput">
+                        <label>Votre nom</label>
+                        <input className="player_name_input_loading" type="text" placeholder="" />
+                    </div>
+                    <div className="blockInput">
+                        <label>Votre mot de passe</label>
+                        <input className="player_mdp_input_loading" type="password" placeholder="" />
+                    </div>
                     <button className="start-button" onClick={loadSave}>
-                        Charger la partie
+                        Charger la partie <i className="fa-solid fa-play"></i>
                     </button>
                 </div>
             ) : (
                 <div className="container-title-page create-save">
-                    <input className="player_name_input_create" type="text" placeholder="Votre nom" />
-                    <input className="player_mdp_input_create" type="text" placeholder="Votre mot de passe" />
+                    <div className="blockInput">
+                        <label>Votre nom</label>
+                        <input className="player_name_input_create" type="text" placeholder="Ex: Jean" />
+                    </div>
+                    <div className="blockInput">
+                        <label>Votre mot de passe</label>
+                        <input className="player_mdp_input_create" type="password" placeholder="Ex: JZK293ne9" />
+                    </div>
+                    <div className="blockInput">
+                        <label>Confirmer votre mot de passe</label>
+                        <input className="player_confirm_mdp_input_create" type="password" placeholder="Ex: JZK293ne9" />
+                    </div>
                     <button className="start-button" onClick={createSave}>
-                        Commencer une nouvelle partie
+                        Commencer une nouvelle partie <i className="fa-solid fa-plus"></i>
                     </button>
                 </div>
             )}
