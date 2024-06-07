0d7be7f6af3f21b3d3aed60f13c8ced343301e1f
Mathis CHAUVIN <mathischauvin256@gmail.com>
2024-06-07 15:03:02 +0200

    add commentary

--- src/client/components/choicePage/choicePage.jsx
+++ src/client/components/choicePage/choicePage.jsx
@@ -3,6 +3,7 @@ import "./choicePage.css"
 export const ChoicePage = ({setSectionID, liste_choix}) => {
     return (
         <>
+            {/* On affiche les choix retourner par l'api  */}
             {liste_choix.map((choix, index) => (
                 <button className="choice-button"  key={index} onClick={() => setSectionID(choix.id_choix)}>
                     { <p>{choix.libelle_choix}</p> }
