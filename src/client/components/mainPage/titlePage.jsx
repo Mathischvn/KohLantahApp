8f10fb6c4b4957ed0da0b122bc35877b6875b62e
Mathis CHAUVIN <mathischauvin256@gmail.com>
2024-06-07 15:15:50 +0200

    commentary

--- src/client/components/mainPage/titlePage.jsx
+++ src/client/components/mainPage/titlePage.jsx
@@ -4,65 +4,65 @@ import { useNavigate } from "react-router-dom"
 import { useState } from "react"
 
 export const TitlePage = () => {
-    const navigate = useNavigate();
-    const [createLoadingSave, setCreateLoadingSave] = useState(false);
-    const [activeButton, setActiveButton] = useState('new');
-    const minLengthPassword = 6;
+    const navigate = useNavigate(); // Hook permettant de naviguer entre les pages
+    const [createLoadingSave, setCreateLoadingSave] = useState(false); // Etat permettant de savoir si l'utilisateur veut créer ou charger une partie
+    const [activeButton, setActiveButton] = useState('new'); 
+    const minLengthPassword = 6; // Longueur minimum du mot de passe
     
-    const createSave = () => {
+    const createSave = () => { // Fonction permettant de créer une partie 
         const inputName = document.querySelector(".player_name_input_create").value;
         const inputMdp = document.querySelector(".player_mdp_input_create").value;
         const errorMessage = document.querySelector(".error-message");
 
         errorMessage.style.opacity = "0";
 
-        if(verifyPasswordPresence()) {
-            if(verifyConfirmPassword()) {
-                if(verifyLengthPassword()) {
-                    const fetchData = async () => { 
+        if(verifyPasswordPresence()) { // Si un mot de passe est présent alors 
+            if(verifyConfirmPassword()) { // On vérifie si le mot de passe et le mot de passe de confirmation sont identiques
+                if(verifyLengthPassword()) { // On vérifie ensuite si la taille du mot de passe est suffisante
+                    const fetchData = async () => { // On envoie une requête pour vérifier si le nom du joueur n'existe pas déjà
                         const response = await fetch(`http://localhost:3000/api/player/insertPlayer/${inputName}/${inputMdp}`);
                         const data = await response.json();
-                        if(data.length === 0) {
-                            document.cookie = `name=${inputName}`;
-                            navigate("/game");
-                        } else {
+                        if(data.length === 0) { // Si le nom n'existe pas alors on crée le joueur
+                            document.cookie = `name=${inputName}`; // On stocke le nom du joueur dans les cookies
+                            navigate("/game"); // On redirige l'utilisateur vers la page du jeu
+                        } else { // Si le nom existe déjà alors on affiche un message d'erreur
                             errorMessage.style.opacity = "1";
                             errorMessage.innerHTML = "Ce nom existe déjà, veuillez prendre un autre nom.";
                         }
                     }
                     fetchData();
-                } else {
+                } else { // Si le mot de passe est trop court alors on affiche un message d'erreur
                     errorMessage.style.opacity = "1";
                     errorMessage.innerHTML = "Mot de passe trop court, mettez au moins 6 caractères.";
                 }
-            } else {
+            } else { // Si les mots de passe ne sont pas identiques alors on affiche un message d'erreur
                 errorMessage.style.opacity = "1";
                 errorMessage.innerHTML = "Mots de passe différents";
             }
-        } else {
+        } else { // Si aucun mot de passe n'est présent alors on affiche un message d'erreur
             errorMessage.style.opacity = "1";
             errorMessage.innerHTML = "Veuillez entrer un mot de passe.";
         }
     };
 
-    const loadSave = () => {
-        const inputName = document.querySelector(".player_name_input_loading").value;
-        const inputMdp = document.querySelector(".player_mdp_input_loading").value;
-        const buttonStart = document.querySelector(".start-button");
+    const loadSave = () => { // Fonction permettant de charger une partie d'un joueur
+        const inputName = document.querySelector(".player_name_input_loading").value; // On récupère le nom du joueur
+        const inputMdp = document.querySelector(".player_mdp_input_loading").value; // On récupère le mot de passe du joueur
+        const buttonStart = document.querySelector(".start-button"); 
         const errorMessage = document.querySelector(".error-message");
         
         errorMessage.style.opacity = "0";
         buttonStart.innerHTML = 'Charger la partie <i class="fa-solid fa-spinner fa-spin-pulse"></i>';
         
-        if(verifyPasswordPresence()) {
+        if(verifyPasswordPresence()) { // Si un mot de passe est présent alors on vérifie si le nom et le mot de passe correspondent à un joueur
             setTimeout(() => {
-                const fetchData = async () => { 
+                const fetchData = async () => { // On envoie une requête pour vérifier si le nom et le mot de passe correspondent à un joueur
                     const response = await fetch(`http://localhost:3000/api/player/getPlayer/${inputName}/${inputMdp}`);
                     const data = await response.json();
-                    if(data.length !== 0) {
+                    if(data.length !== 0) { // Si le joueur existe et possède un partie alors on redirige l'utilisateur vers la page du jeu
                         document.cookie = `name=${inputName}`;
                         navigate("/game");
-                    } else {
+                    } else { // Si le joueur n'existe pas alors on affiche un message d'erreur
                         errorMessage.style.opacity = "1";
                         errorMessage.innerHTML = "Nom ou mot de passe incorrect";
                     }
@@ -77,7 +77,7 @@ export const TitlePage = () => {
         }
     };
 
-    const verifyPasswordPresence = () => {
+    const verifyPasswordPresence = () => { // Fonction permettant de vérifier si un mot de passe a été entré
         let inputMdp;
         document.querySelector(".player_mdp_input_create") !== null ? 
             inputMdp = document.querySelector(".player_mdp_input_create").value 
@@ -90,7 +90,7 @@ export const TitlePage = () => {
         return presencePassword;
     };
 
-    const verifyConfirmPassword = () => {
+    const verifyConfirmPassword = () => { // Fonction permettant de vérifier si le mot de passe et le mot de passe de confirmation sont identiques
         const inputMdp = document.querySelector(".player_mdp_input_create").value;
         const inputConfirmMdp = document.querySelector(".player_confirm_mdp_input_create").value;
         let samePassword = true;
@@ -100,7 +100,7 @@ export const TitlePage = () => {
         return samePassword;
     };
 
-    const verifyLengthPassword = () => {
+    const verifyLengthPassword = () => { // Fonction permettant de vérifier si la taille du mot de passe est suffisante
         const inputMdp = document.querySelector(".player_mdp_input_create").value;
         let passWordSafe = true;
         if(inputMdp.length < minLengthPassword) {
